dfb3d40f4cf3ff66d29982353b8d64564ec35e68
hirabatool191 <batool.hira2020@gmail.com>
2026-03-04 17:31:41 -0800

    push code

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-const mongoURL = "mongodb://127.0.0.1:27017/mydb"; 
-
-mongoose.connect(mongoURL)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch(err => console.error("❌ MongoDB connection error:", err));
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("DB Error:", err));
