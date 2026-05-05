adedb9134b14c1eff5484cf3cfdfebdfbbea8a55
hirabatool191 <batool.hira2020@gmail.com>
2026-05-05 06:20:42 -0700

    push request

--- seed.js
+++ seed.js
@@ -1,19 +1,23 @@
 const mongoose = require("mongoose");
 const Product = require("./models/Product");
-const products = require("./data/products.json"); // 🔥 IMPORT JSON
 
-mongoose.connect("mongodb+srv://username:password@cluster0.xxxxx.mongodb.net/myDatabase")
+// 🔥 JSON import
+const products = require("./data/products.json"); // path check kar lena
+
+mongoose.connect("mongodb+srv://batoolhira2020_db_user:p2a8tGRzXkmsNbQm@cluster0.awwluuv.mongodb.net/smhXpress?retryWrites=true&w=majority")
   .then(() => console.log("DB Connected"))
   .catch(err => console.log(err));
 
 const run = async () => {
   try {
     await Product.deleteMany();
-    await Product.insertMany(products);
 
-    console.log("All products inserted from JSON file");
+    // 🔥 insert JSON data
+    const res = await Product.insertMany(products);
+
+    console.log("Inserted:", res.length);
   } catch (err) {
-    console.log("Seed error:", err);
+    console.log("Seed Error:", err);
   } finally {
     mongoose.connection.close();
   }
