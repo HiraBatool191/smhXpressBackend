799da78ae76343fd8fcefcb012662a9364f839ec
minahil-1234 <afzaalminahil0@gmail.com>
2026-05-05 18:23:49 +0500

    push request

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -7,8 +5,7 @@ require("./db");
 
 const app = express();
 
-// middlewares
-app.use(cors()); // 🔥 missing tha tumhare code me
+app.use(cors()); 
 app.use(express.json());
 
 
@@ -20,7 +17,7 @@ const sendOtpRoute = require("./routes/otp");
 const verifyOtpRoute = require("./routes/verifyOtp");
 const resetPasswordRoute = require("./routes/resetPassword");
 const dashboardRoute = require("./routes/dashboard");
-const cartRoute = require("./routes/cart"); // 🔥 ADD HERE
+const cartRoute = require("./routes/cart");
 
 
 // ========== ROUTE MIDDLEWARE ==========
@@ -28,7 +25,7 @@ app.use("/api/signup", signupRoute);
 app.use("/api/login", loginRoute);
 app.use("/api/products", productsRoute);
 app.use("/api/dashboard", dashboardRoute);
-app.use("/api/cart", cartRoute); // 🔥 CLEAN PLACE
+app.use("/api/cart", cartRoute); 
 app.use("/api/send-otp", sendOtpRoute);
 app.use("/api/verify-otp", verifyOtpRoute);
 app.use("/api/reset-password", resetPasswordRoute);
--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,25 @@
+// const mongoose = require("mongoose");
+
+// const productSchema = new mongoose.Schema({
+//   id: Number,
+//   name: String,
+//   price: Number,
+//   category: String,
+//   description: String,
+//   image: String,
+//   views: {
+//     type: Number,
+//     default: 0,
+//   },
+// });
+
+// module.exports = mongoose.model("Product", productSchema);
+
+
+
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  id: Number,
   name: String,
   price: Number,
   category: String,
