63b1a044cb5a620f3751490dff0722d33b8916d4
minahil-1234 <afzaalminahil0@gmail.com>
2026-05-09 17:42:13 +0500

    push request

--- models/Product.js
+++ models/Product.js
@@ -1,20 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const productSchema = new mongoose.Schema({
-//   id: Number,
-//   name: String,
-//   price: Number,
-//   category: String,
-//   description: String,
-//   image: String,
-//   views: {
-//     type: Number,
-//     default: 0,
-//   },
-// });
-
-// module.exports = mongoose.model("Product", productSchema);
-
 
 
 const mongoose = require("mongoose");
